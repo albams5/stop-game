9bc81e79b1c766af922071c33e0db22f1c0f32a8
Alba Martin <albams51289@gmail.com>
2023-12-26 11:02:16 +0100

    Stop game finished

---
+++ app.js
@@ -0,0 +1,98 @@
+function saveUsername(){
+  const inputUsername = document.getElementById("input-username");
+  const usernameValue = inputUsername.value;
+  if(usernameValue){
+    playerName = document.getElementById("player-name");
+    startGame = document.getElementById("div-start-game");
+    playerName.classList.add("hide");
+    startGame.classList.remove("hide");
+    localStorage.setItem("username", JSON.stringify(usernameValue));
+  } else{
+    alert("Please introduce an username");
+  }
+}
+document.getElementById("btn-to-start").addEventListener("click", saveUsername);
+
+function showBtn(){
+    gameDisplay = document.getElementById("game-display");
+    divGame = document.getElementById("btn-game-display");
+    gameDisplay.classList.add("hide");
+    divGame.classList.remove("hide");
+    const buttonMoving = document.getElementById("click-to-win");
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    const classBtn = "clase-"+randomNumber;
+    buttonMoving.classList.add(classBtn);
+    countSeconds();
+}
+
+function removeLastPlayer(){
+  const divUserScore = document.getElementById("user-scores");
+  const listUsers = divUserScore.querySelectorAll('.username-in-score');
+    const listScores = divUserScore.querySelectorAll(".currently-playing");
+    if (listUsers.length > 5) {
+      for (let i = 5; i < listUsers.length; i++) {
+      divUserScore.removeChild(listUsers[i]);
+      divUserScore.removeChild(listScores[i]);
+    }
+  }
+}
+
+
+function startGame(){
+    startGame = document.getElementById("div-start-game");
+    gameDisplay = document.getElementById("game-display");
+    startGame.classList.add("hide");
+    gameDisplay.classList.remove("hide");
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    setTimeout(showBtn, randomNumber*1000);
+    const username = JSON.parse(localStorage.getItem("username"));
+    const userNameInScore = document.createElement("p");
+    userNameInScore.classList.add("username-in-score");
+    userNameInScore.textContent = username;
+    const divUserScore = document.getElementById("user-scores");
+    const currentlyPlaying = document.createElement("p");
+    currentlyPlaying.classList.add("currently-playing");
+    currentlyPlaying.textContent = "Currently playing...";
+    divUserScore.insertBefore(currentlyPlaying, divUserScore.firstChild);
+    divUserScore.insertBefore(userNameInScore, divUserScore.firstChild);
+    removeLastPlayer();
+}
+document.getElementById("to-game-btn").addEventListener("click", startGame);
+
+function countSeconds(){
+    let seconds = 0;
+    let stop = false;
+
+  function addingSeconds() {
+    if(!stop){
+        seconds++;
+    console.log(seconds);
+    setTimeout(addingSeconds, 1000);
+    }
+  }
+
+  addingSeconds();
+  document.getElementById("click-to-win").addEventListener("click", ()=>{
+    stop = true;
+    localStorage.setItem("seconds", JSON.stringify(seconds));
+    divGame = document.getElementById("btn-game-display");
+    yourScore = document.getElementById("your-score");
+    reactionTime = document.getElementById("total-time");
+    divGame.classList.add("hide");
+    yourScore.classList.remove("hide");
+    reactionTime.textContent = localStorage.getItem("seconds");
+    const currentlyPlaying = document.querySelector(".currently-playing");
+    currentlyPlaying.textContent = localStorage.getItem("seconds") + " seconds";
+
+  });
+}
+
+function reloadGame(){
+  yourScore = document.getElementById("your-score");
+  playerName = document.getElementById("player-name");
+  const inputUsername = document.getElementById("input-username");
+  inputUsername.value = "";
+  yourScore.classList.add("hide");
+  playerName.classList.remove("hide");
+}
+document.getElementById("play-again-btn").addEventListener("click", reloadGame)
